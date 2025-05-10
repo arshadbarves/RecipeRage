35587df339d13554f0821564762324d4f82a52b5
Arshad Barves <arshad.barves@kwalee.com>
2025-05-10 21:01:30 +0530

    Remove duplicate creation options from RecipeRageSetupManager

--- Assets/Editor/Setup/RecipeRageSetupManager.cs
+++ Assets/Editor/Setup/RecipeRageSetupManager.cs
@@ -1,6 +1,6 @@
-using System.IO;
 using UnityEditor;
 using UnityEngine;
+using RecipeRage.Editor.Prefabs;
 
 namespace RecipeRage.Editor
 {
@@ -10,32 +10,8 @@ namespace RecipeRage.Editor
     /// </summary>
     public class RecipeRageSetupManager : EditorWindow
     {
-        // References to generator scripts
-        private IngredientGenerator _ingredientGenerator;
-        private RecipeGenerator _recipeGenerator;
-        private PrefabGenerator _prefabGenerator;
-        private SceneSetupGenerator _sceneSetupGenerator;
-        private GameModeGenerator _gameModeGenerator;
-        private CharacterClassGenerator _characterClassGenerator;
-        private StationGenerator _stationGenerator;
-
-        // Setup options
-        private bool _generateIngredients = true;
-        private bool _generateRecipes = true;
-        private bool _generatePrefabs = true;
-        private bool _generateGameModes = true;
-        private bool _generateCharacterClasses = true;
-        private bool _generateStations = true;
-        private bool _setupScene = true;
-
-        // Paths
-        private const string INGREDIENTS_PATH = "Assets/ScriptableObjects/Ingredients";
-        private const string RECIPES_PATH = "Assets/ScriptableObjects/Recipes";
-        private const string GAME_MODES_PATH = "Assets/ScriptableObjects/GameModes";
-        private const string CHARACTER_CLASSES_PATH = "Assets/ScriptableObjects/CharacterClasses";
-        private const string PREFABS_PATH = "Assets/Prefabs";
-        private const string STATIONS_PATH = "Assets/Prefabs/Stations";
-        private const string SCENES_PATH = "Assets/Scenes";
+        // No generator scripts or setup options needed anymore
+        // All functionality is delegated to PrefabCreationManager
 
         [MenuItem("RecipeRage/Setup Manager")]
         public static void ShowWindow()
@@ -43,210 +19,60 @@ namespace RecipeRage.Editor
             GetWindow<RecipeRageSetupManager>("RecipeRage Setup");
         }
 
-        private void OnEnable()
-        {
-            // Initialize generators
-            _ingredientGenerator = new IngredientGenerator();
-            _recipeGenerator = new RecipeGenerator();
-            _prefabGenerator = new PrefabGenerator();
-            _sceneSetupGenerator = new SceneSetupGenerator();
-            _gameModeGenerator = new GameModeGenerator();
-            _characterClassGenerator = new CharacterClassGenerator();
-            _stationGenerator = new StationGenerator();
-        }
+        // No initialization needed
 
         private void OnGUI()
         {
             GUILayout.Label("RecipeRage Setup Manager", EditorStyles.boldLabel);
             EditorGUILayout.Space();
 
-            EditorGUILayout.HelpBox("This tool will generate all necessary assets for the RecipeRage game. " +
-                                   "You can choose which components to generate.", MessageType.Info);
+            EditorGUILayout.HelpBox("This tool will generate all necessary assets for the RecipeRage game using the centralized PrefabCreationManager.",
+                                   MessageType.Info);
 
             EditorGUILayout.Space();
 
-            // Setup options
-            _generateIngredients = EditorGUILayout.Toggle("Generate Ingredients", _generateIngredients);
-            _generateRecipes = EditorGUILayout.Toggle("Generate Recipes", _generateRecipes);
-            _generatePrefabs = EditorGUILayout.Toggle("Generate Prefabs", _generatePrefabs);
-            _generateGameModes = EditorGUILayout.Toggle("Generate Game Modes", _generateGameModes);
-            _generateCharacterClasses = EditorGUILayout.Toggle("Generate Character Classes", _generateCharacterClasses);
-            _generateStations = EditorGUILayout.Toggle("Generate Stations", _generateStations);
-            _setupScene = EditorGUILayout.Toggle("Setup Scene", _setupScene);
+            EditorGUILayout.HelpBox("All assets will be created with a single click, including:\n" +
+                                   "• Manager prefabs (GameBootstrap, NetworkManager, etc.)\n" +
+                                   "• UI prefabs (SplashScreenManager, LoadingScreenManager, etc.)\n" +
+                                   "• Player prefabs\n" +
+                                   "• Station prefabs\n" +
+                                   "• Scriptable objects (Character Classes, Game Modes)\n" +
+                                   "• Audio assets\n" +
+                                   "• Scene setup",
+                                   MessageType.Info);
 
             EditorGUILayout.Space();
 
-            // Generate button
-            if (GUILayout.Button("Generate All"))
+            // Generate button with a larger height to make it more prominent
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+
+            if (GUILayout.Button("Generate All Game Assets", GUILayout.Height(40), GUILayout.Width(250)))
             {
                 GenerateAll();
             }
 
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+
             EditorGUILayout.Space();
 
-            // Individual generation buttons
-            EditorGUILayout.BeginHorizontal();
-
-            if (GUILayout.Button("Generate Ingredients"))
-            {
-                GenerateIngredients();
-            }
-
-            if (GUILayout.Button("Generate Recipes"))
-            {
-                GenerateRecipes();
-            }
-
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.BeginHorizontal();
-
-            if (GUILayout.Button("Generate Prefabs"))
-            {
-                GeneratePrefabs();
-            }
-
-            if (GUILayout.Button("Generate Game Modes"))
-            {
-                GenerateGameModes();
-            }
-
-            if (GUILayout.Button("Generate Character Classes"))
-            {
-                GenerateCharacterClasses();
-            }
-
-            if (GUILayout.Button("Generate Stations"))
-            {
-                GenerateStations();
-            }
-
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.BeginHorizontal();
-
-            if (GUILayout.Button("Setup Scene"))
-            {
-                SetupScene();
-            }
-
-            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.HelpBox("Note: Individual generation options have been removed to avoid confusion. " +
+                                   "All assets are now created through the centralized PrefabCreationManager.",
+                                   MessageType.Warning);
         }
 
+
+
         /// <summary>
-        /// Generate all selected components.
+        /// Generate all game assets using the centralized PrefabCreationManager.
         /// </summary>
         private void GenerateAll()
         {
             // Use the centralized PrefabCreationManager for one-click setup
-            RecipeRage.Editor.Prefabs.PrefabCreationManager.CreateAllGameAssets();
+            PrefabCreationManager.CreateAllGameAssets();
 
             Debug.Log("RecipeRage setup completed successfully!");
-        }
-
-        /// <summary>
-        /// Create necessary directories for assets.
-        /// </summary>
-        private void CreateDirectories()
-        {
-            CreateDirectoryIfNotExists(INGREDIENTS_PATH);
-            CreateDirectoryIfNotExists(RECIPES_PATH);
-            CreateDirectoryIfNotExists(GAME_MODES_PATH);
-            CreateDirectoryIfNotExists(CHARACTER_CLASSES_PATH);
-            CreateDirectoryIfNotExists(PREFABS_PATH);
-            CreateDirectoryIfNotExists(STATIONS_PATH);
-            CreateDirectoryIfNotExists(SCENES_PATH);
-
-            // Create subdirectories for prefabs
-            CreateDirectoryIfNotExists(Path.Combine(PREFABS_PATH, "Ingredients"));
-            CreateDirectoryIfNotExists(Path.Combine(PREFABS_PATH, "Stations"));
-            CreateDirectoryIfNotExists(Path.Combine(PREFABS_PATH, "UI"));
-            CreateDirectoryIfNotExists(Path.Combine(PREFABS_PATH, "Player"));
-        }
-
-        /// <summary>
-        /// Create a directory if it doesn't exist.
-        /// </summary>
-        /// <param name="path">The directory path.</param>
-        private void CreateDirectoryIfNotExists(string path)
-        {
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-                Debug.Log($"Created directory: {path}");
-            }
-        }
-
-        /// <summary>
-        /// Generate ingredient scriptable objects.
-        /// </summary>
-        private void GenerateIngredients()
-        {
-            // Use the centralized PrefabCreationManager
-            Debug.Log("Using centralized PrefabCreationManager for ingredient generation...");
-            RecipeRage.Editor.Prefabs.PrefabCreationManager.CreateAllScriptableObjects();
-        }
-
-        /// <summary>
-        /// Generate recipe scriptable objects.
-        /// </summary>
-        private void GenerateRecipes()
-        {
-            // Use the centralized PrefabCreationManager
-            Debug.Log("Using centralized PrefabCreationManager for recipe generation...");
-            RecipeRage.Editor.Prefabs.PrefabCreationManager.CreateAllScriptableObjects();
-        }
-
-        /// <summary>
-        /// Generate prefabs for the game.
-        /// </summary>
-        private void GeneratePrefabs()
-        {
-            // Use the centralized PrefabCreationManager
-            Debug.Log("Using centralized PrefabCreationManager for prefab generation...");
-            RecipeRage.Editor.Prefabs.PrefabCreationManager.CreateAllManagerPrefabs();
-            RecipeRage.Editor.Prefabs.PrefabCreationManager.CreateAllUIPrefabs();
-            RecipeRage.Editor.Prefabs.PrefabCreationManager.CreatePlayerPrefab();
-        }
-
-        /// <summary>
-        /// Generate game mode scriptable objects.
-        /// </summary>
-        private void GenerateGameModes()
-        {
-            // Use the centralized PrefabCreationManager
-            Debug.Log("Using centralized PrefabCreationManager for game mode generation...");
-            RecipeRage.Editor.Prefabs.PrefabCreationManager.CreateAllScriptableObjects();
-        }
-
-        /// <summary>
-        /// Generate character class scriptable objects.
-        /// </summary>
-        private void GenerateCharacterClasses()
-        {
-            // Use the centralized PrefabCreationManager
-            Debug.Log("Using centralized PrefabCreationManager for character class generation...");
-            RecipeRage.Editor.Prefabs.PrefabCreationManager.CreateAllScriptableObjects();
-        }
-
-        /// <summary>
-        /// Generate cooking station prefabs.
-        /// </summary>
-        private void GenerateStations()
-        {
-            // Use the centralized PrefabCreationManager
-            Debug.Log("Using centralized PrefabCreationManager for station generation...");
-            RecipeRage.Editor.Prefabs.PrefabCreationManager.CreateAllStationPrefabs();
-        }
-
-        /// <summary>
-        /// Setup the game scene.
-        /// </summary>
-        private void SetupScene()
-        {
-            // Use the centralized PrefabCreationManager
-            Debug.Log("Using centralized PrefabCreationManager for scene setup...");
-            RecipeRage.Editor.Prefabs.PrefabCreationManager.SetupAllScenes();
         }
     }
 }
