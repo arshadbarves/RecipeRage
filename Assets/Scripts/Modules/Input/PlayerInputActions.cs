a333f92d21930ec79607670321b13f64ae138d13
Arshad Barves <39230484+arshadbarves@users.noreply.github.com>
2026-01-08 21:48:29 +0530

    refactor(input): Migrate Core.Input to Modules.Input

--- Assets/Scripts/Modules/Input/PlayerInputActions.cs
+++ Assets/Scripts/Modules/Input/PlayerInputActions.cs
@@ -15,7 +15,7 @@ using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
 
-namespace Core.Input
+namespace Modules.Input
 {
     /// <summary>
     /// Provides programmatic access to <see cref="InputActionAsset" />, <see cref="InputActionMap" />, <see cref="InputAction" /> and <see cref="InputControlScheme" /> instances defined in asset "Assets/Settings/PlayerInputActions.inputactions".
